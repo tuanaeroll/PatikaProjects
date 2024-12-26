86fc2300781a123a1f9967afd078251a73f78b31
tuanaeroll <erolltuana@gmail.com>
2024-12-26 18:28:24 +0300

    files are fixed

---
+++ For/Program.cs
@@ -0,0 +1,40 @@
+﻿// For yapısı
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine("Kendime inaniyorum, ben bu yazilim isini hallederim");
+}
+
+for (int i = 1; i < 21; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 2; i < 21; i += 2)
+{
+    Console.WriteLine(i);
+}
+
+int sum = 0;
+for (int i = 50; i <= 150; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"50 ile 150 arasındaki sayıların toplamı: {sum}");
+
+int oddSum = 0;
+int evenSum = 0;
+
+for (int i = 1; i <= 120; i++)
+{
+    if (i % 2 == 0)
+    {
+        evenSum += i;
+    }
+    else
+    {
+        oddSum += i;
+    }
+}
+
+Console.WriteLine($"1 ile 120 arasındaki tek sayıların toplamı: {oddSum}");
+Console.WriteLine($"1 ile 120 arasındaki çift sayıların toplamı: {evenSum}");
---
+++ While/Program.cs
@@ -0,0 +1,57 @@
+﻿// 1 -> Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+int i = 0;
+while (i < 10)
+{
+    Console.WriteLine("Ben bu yazilim isini hallederim!!!");
+    i++;
+}
+// 2 -> 1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+int a = 1;
+while (a <= 20)
+{
+    Console.WriteLine(a);
+    a++;
+}
+
+// 3 -> 1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+int b = 1;
+while (b < 20)
+{
+    if (b % 2 == 0)
+    {
+        Console.WriteLine(b);
+    }
+    b++;
+}
+
+// 4 -> 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+int c = 50;
+int sum = 0;
+
+while (c <= 150)
+{
+    sum += c;
+    c++;
+}
+
+Console.WriteLine(sum);
+
+// 5 -> 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+int d = 1;
+int sumOdd = 0;
+int sumEven = 0;
+
+while (d < 120)
+{
+    if (d % 2 == 0)
+    {
+        sumEven += d;
+    }
+    else
+    {
+        sumOdd += d;
+    }
+    d++;
+}
+Console.WriteLine($"Çift sayıların toplamı:{sumEven}");
+Console.WriteLine($"Çift sayıların toplamı:{sumOdd}");
