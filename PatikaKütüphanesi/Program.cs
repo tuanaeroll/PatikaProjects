e9e5390697edb4a257a234e116704a1d4f516fa1
tuanaeroll <erolltuana@gmail.com>
2025-01-07 00:40:48 +0300

    Constructor2 is done.

---
+++ PatikaKütüphanesi/Program.cs
@@ -0,0 +1,19 @@
+﻿using PatikaKütüphanesi;
+
+Kitap kitap1 = new Kitap("1984", "George Orwell", "Can Yayınları", 352);
+Kitap kitap2 = new Kitap("Sineklerin Tanrısı", "William Golding", "İthaki Yayınları", 408);
+
+Kitap kitap3 = new Kitap();
+kitap3.Ad = "Hayvan Çiftliği";
+kitap3.YazarAdi = "George Orwell";
+kitap3.YayinEvi = "Can Yayınları";
+kitap3.SayfaSayisi = 152;
+kitap3.Olustur();
+
+Kitap kitap4 = new Kitap();
+kitap4.Ad = "Körlük";
+kitap4.YazarAdi = "Jose Saramago";
+kitap4.YayinEvi = "Kırmızı Kedi Yayınları";
+kitap4.SayfaSayisi = 312;
+kitap4.Olustur();
+
