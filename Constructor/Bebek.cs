1ff1f5bff5d70978867ab8d7466393f28a51cc17
tuanaeroll <erolltuana@gmail.com>
2025-01-06 23:00:35 +0300

    Constructor is done.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    // Bebek sınıfını tanımlıyoruz.
+    public class Bebek
+    {
+        // Bebek sınıfının özelliklerini tanımlıyoruz. Bunlara erişilmesi için get ve set metodlarını ekliyoruz. 
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; private set; }
+
+        public Bebek(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Dogum();
+        }
+
+        public Bebek() 
+        {
+            Dogum();
+        }
+
+        public void Dogum ()  
+        {
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+            Console.WriteLine($"{Ad} {Soyad} bebek, {DogumTarihi}'nde dünyaya geldi.");
+        }
+
+    }
+}
---
+++ Constructor1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Constructor1;
+
+Bebek bebek1 = new Bebek();
+bebek1.Ad = "Chandler";
+bebek1.Soyad = "Bing";
+Console.WriteLine($"{bebek1.Ad} {bebek1.Soyad}");
+
+Bebek bebek2 = new Bebek("Monica", "Geller");
+Console.WriteLine($"{bebek2.Ad} {bebek2.Soyad}");
+
