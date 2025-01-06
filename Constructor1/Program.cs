6e37324ab4bce447c604da4b58812f94bb6e4544
tuanaeroll <erolltuana@gmail.com>
2025-01-06 23:04:51 +0300

    Constructor is done.

--- Constructor1/Program.cs
+++ Constructor1/Program.cs
@@ -4,7 +4,9 @@ Bebek bebek1 = new Bebek();
 bebek1.Ad = "Chandler";
 bebek1.Soyad = "Bing";
 Console.WriteLine($"{bebek1.Ad} {bebek1.Soyad}");
+bebek1.Dogum();
 
 Bebek bebek2 = new Bebek("Monica", "Geller");
 Console.WriteLine($"{bebek2.Ad} {bebek2.Soyad}");
+bebek2.Dogum();
 
