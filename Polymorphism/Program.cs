dc615c47bf9ddae78548198f0addb5c797e931a2
tuanaeroll <erolltuana@gmail.com>
2025-01-10 15:57:26 +0300

    Polymorphism is done.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿using Polymorphism;
+
+BaseGeometrikSekil kare = new Kare(5,5);
+kare.AlanHesapla();
+
+BaseGeometrikSekil dikdortgen = new Dikdortgen(5, 10);
+dikdortgen.AlanHesapla();
+
+BaseGeometrikSekil dikUcgen = new DikUcgen(5,5);
+dikUcgen.AlanHesapla();
