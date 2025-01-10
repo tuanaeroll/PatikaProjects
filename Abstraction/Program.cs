cd1f7b643b1e4067539989d1f6449dd4ce7dd60c
tuanaeroll <erolltuana@gmail.com>
2025-01-10 18:24:33 +0300

    abstract practice is done.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,10 @@
+﻿using Abstraction;
+
+Calisan doktor = new Doktor("Ajda", "Pekkan", "Estetik");
+doktor.Gorev();
+
+Calisan ogretmen = new Ogretmen("Zeki", "Müren", "Müzik");
+ogretmen.Gorev();
+
+Calisan mimar = new Mimar("Sezen", "Aksu", "İç Mimarlık");
+mimar.Gorev();
