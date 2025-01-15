c4a22b1c50b1d310781a73b0de2c327ad1a9c6b9
tuanaeroll <erolltuana@gmail.com>
2025-01-15 16:55:55 +0300

    lists1 pratice is done

---
+++ Lists1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Davetlilerimiz\n");
+
+List<string> davetliler = new List<string>();
+davetliler.Add("Bülent Ersoy");
+davetliler.Add("Ajda Pekkan");
+davetliler.Add("Ebru Gündeş");
+davetliler.Add("Hadise");
+davetliler.Add("Hande Yener");
+davetliler.Add("Tarkan");
+davetliler.Add("Funda Arar");
+davetliler.Add("Demet Akalın");
+
+foreach (string davetli in davetliler)
+{
+    Console.WriteLine(davetli);
+}
