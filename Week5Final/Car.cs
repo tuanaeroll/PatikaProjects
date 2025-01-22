5c3829919a5d559182f8a1716a4dfacaabd8f612
tuanaeroll <erolltuana@gmail.com>
2025-01-23 01:00:51 +0300

    finish week5 final

---
+++ Week5Final/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace Week5Final
+{
+    public class Car
+    {
+        public DateTime ProductionDate { get;private set; }
+        public int SerialNumber { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int NumberOfDoors { get; set; }
+
+        public Car() 
+        {
+            ProductionDate = DateTime.Now;
+        }
+    }
+}
