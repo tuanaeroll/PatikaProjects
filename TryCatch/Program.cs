51971000be6d9d2845a31a51e9bdb5accaf34ffc
Tuana <erolltuana@gmail.com>
2025-01-22 23:56:13 +0300

    Update Program.cs

--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -1,7 +1,3 @@
-﻿//Kullanıcıdan bir sayı girmesini isteyen bir program yazın. Kullanıcı geçerli bir sayı girdiğinde bu sayının karesini ekrana yazdırınız. 
-
-//Kullanıcı geçersiz bir giriş yaparsa (sayı yerine harf veya sembol girmesi gibi), "Geçersiz giriş! Lütfen bir sayı giriniz." şeklinde bir hata mesajı gösterini<
-
 bool IsValid = false;
 
 while (!IsValid)
