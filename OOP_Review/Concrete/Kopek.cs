234e45b44d59a2ef61715f3815071f73ca5e2dfd
tuanaeroll <erolltuana@gmail.com>
2025-01-24 23:38:25 +0300

    added OOP review.

---
+++ OOP_Review/Concrete/Kopek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP_Review.Abstract;
+
+namespace OOP_Review.Concrete
+{
+    internal class Kopek : BaseHayvan
+    {
+        public override void SesCikar()
+        {
+            Console.WriteLine(Ad + "Hav hav hav");
+        }
+    }
+}
