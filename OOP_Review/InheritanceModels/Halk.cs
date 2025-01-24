56a1a0a15419c95856c52677b1d4ea35bea00466
tuanaeroll <erolltuana@gmail.com>
2025-01-24 20:38:19 +0300

    added OOP review.

---
+++ OOP_Review/InheritanceModels/Halk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Review.InheritanceModels
+{
+    internal class Halk : BaseKatilimci
+    {
+        // Diğer tüm katılımcılarda çalışan Base de tanımlanan RöportajVer() metodunun Halk classında da çalışmasını ama farklı çalışmasını istiyoruz.
+
+        // POLYMORPHISM (Çok Biçimlilik) -> Virtual / Override
+
+        public override void RoportajVer()
+        {
+            Console.WriteLine("Altın kelebek çok bozdu.");
+
+            // Alttaki kodu tutarsak gidip base deki metodu çalıştırır. Bazen hem burda içeride değişiklik yapacağız hem de base metodunu kullanacağız.
+            // base.RoportajVer();
+        }
+    }
+}
---
+++ OOP_Review/InheritanceModels/Sarkici.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Review.InheritanceModels
+{
+    internal class Sarkici : BaseKatilimci
+    {
+        public string SonAlbum { get; set; }
+
+        public void SarkiSoyle()
+        {
+            Console.WriteLine("Lay la lay ...");
+        }
+    }
+}
