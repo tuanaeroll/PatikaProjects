5d1731c9ff338e36f76913f6ce468ed395f147d3
tuanaeroll <erolltuana@gmail.com>
2025-02-03 00:54:16 +0300

    LINQ practice1 is done.

---
+++ LINQ1/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> numbers = new List<int> { 5, 10, 15, 20, -5, -10, -15, -20};
+
+var evenNumbers = numbers.Where(x => x % 2 == 0);
+foreach (var number in evenNumbers){ Console.WriteLine(number); }
+Console.WriteLine("----------------------------");
+
+var oddNumbers = numbers.Where(x => x % 2 != 0);
+foreach (var number in oddNumbers){ Console.WriteLine(number); }
+Console.WriteLine("----------------------------");
+
+var negativeNumbers = numbers.Where(x => x < 0);
+foreach (var number in negativeNumbers) { Console.WriteLine(number); }
+Console.WriteLine("----------------------------");
+
+var positiveNumbers = numbers.Where(x => x > 0);
+foreach (var number in positiveNumbers) { Console.WriteLine(number); }
+Console.WriteLine("----------------------------");
+
+var specificNumbers = numbers.Where(x => x > 15 && x < 22);
+foreach (var number in specificNumbers) { Console.WriteLine(number); }
+Console.WriteLine("----------------------------");
+
+var squaredNumbers = numbers.Select(x => x * x);
+foreach (var number in squaredNumbers){ Console.WriteLine(number); }
+
+
