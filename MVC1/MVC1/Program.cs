1918aa5211930a467a16f09595a23f4e8a5116ad
tuanaeroll <erolltuana@gmail.com>
2025-02-06 02:34:34 +0300

    fixed models

--- MVC1/MVC1/Program.cs
+++ MVC1/MVC1/Program.cs
@@ -5,10 +5,25 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// wwwroot klasöründeki  statik dosyaların tarayıcan erişilebilir olmasını sağlayaan metot
+app.UseStaticFiles();
+
+// Routing, tarayıcının yaptığı istekleri doğru Contoller-Action metodlarına yönlendirir, bu satır da bu erişimi sağlar
+app.UseRouting();
+
 // Projeye hangi şekilde istek atılacağını MappControllerRoute metodu ile tanımlıyoruz.
 app.MapControllerRoute(
     name : "default",
-    pattern : "{controller=Home}/{action=Index}"
+    pattern : "{controller=Home}/{action=Index}/{id?}"
     );
 
 app.Run();
+
+// Controller -> Kullanıcıdan gelen isteklere karşılık veren sınıflar. İş mantığının yazıldığı yerdir.
+// Action -> Controller içinde tanımlanan metodlardır.
+// View -> Kullanıcıya sunulan HTML tabanlı arayüzlerdir.
+// Razor -> C# kodu ile HTML’i iç içe yazmaya izin veren bir şablon dilidir.
+// RazorView -> .cshtml uzantılı HTML + Razor kodlarını içeren dosyalardır.
+// wwwroot -> statik dosyaların tutulduğu klasördür.
+// builder.Build() -> Uygulamayı oluşturur ve gerekli bağımlılıkları yükler.
+// app.Run() -> Uygulamayı başlatır ve HTTP isteklerini dinlemeye başlar.
