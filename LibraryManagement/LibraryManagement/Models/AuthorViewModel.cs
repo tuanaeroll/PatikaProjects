8ab0f90b1c1c3f15918d96931809d5b804546f1b
tuanaeroll <erolltuana@gmail.com>
2025-02-23 21:12:10 +0300

    Add changes for tracking large files with Git LFS

---
+++ LibraryManagement/LibraryManagement/Models/AuthorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagement.Models
+{
+    public class AuthorViewModel
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<string> Books { get; set; }
+    }
+}
